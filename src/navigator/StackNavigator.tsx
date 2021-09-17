063b96c7ada299172a775c4e93ae2ecaf2d1865a
joaquinbian <joacobian21@gmail.com>
2021-09-16 23:10:58 -0300

    adding a condition to show permissions or map view

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import MapScreen from '../screens/MapScreen';
 import PermissionsScreen from '../screens/PermisisonsScreen';
+import {PermissionsContext} from '../context/permissionsContext';
+import LoadingSreen from '../screens/LoadingSreen';
 
 const StackNavigator = () => {
   const Stack = createStackNavigator();
+  const {permissions} = useContext(PermissionsContext);
+  const {locationStatus} = permissions;
+
+  if (locationStatus === 'unavailable') {
+    return <LoadingSreen />;
+  }
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}>
-      <Stack.Screen name="permissions" component={PermissionsScreen} />
-      <Stack.Screen name="map" component={MapScreen} />
+      {/* dependiendo del status del permiso, se muestra una pantalla u otra */}
+      {locationStatus === 'granted' ? (
+        <Stack.Screen name="map" component={MapScreen} />
+      ) : (
+        <Stack.Screen name="permissions" component={PermissionsScreen} />
+      )}
     </Stack.Navigator>
   );
 };
